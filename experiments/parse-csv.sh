017f2ef56f9814e3d69a7b4e7d10a20db5bdd36d
Vincent Bloemen <v.bloemen@utwente.nl>
2016-01-06 13:13:59 +0100

    Add script to extract re-exploration info to table

--- experiments/parse-csv.sh
+++ experiments/parse-csv.sh
@@ -1,5 +1,5 @@
 
-
+FILE="synthetic"
 ## create the speedup graph for a given model
 #python csv2graph.py results-paper/results-beem.csv bakery.6 tarjan 1 > tmp.tex
 
@@ -9,9 +9,12 @@
 ## create a table of the results
 #python csv2table.py results-paper/results-beem-selected.csv > tmp.tex
 
+## create a table of the memory usage (re-exploration)
+python csv2memtable.py results-paper/results-${FILE}.csv > ${FILE}-memtable.tex
+
 # create a scatterplot of the results
-python csv2scatter.py results-paper/results-beem.csv tarjan 1 ufscc 64 > tmp.tex
+#python csv2scatter.py results-paper/results-beem.csv tarjan 1 ufscc 64 > tmp.tex
 
 ## compile the latex file
-pdflatex -synctex=1 -interaction=nonstopmode tmp.tex ;    
+pdflatex -synctex=1 -interaction=nonstopmode ${FILE}-memtable.tex ;    
 
